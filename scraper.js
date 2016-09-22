b48527a6613dd71cc17fb12c71f12d886a9afe07
nesbit <nesbit@users.noreply.github.com>
2016-09-21 21:13:17 -0700

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 var request = require("request");
 var parser = require('xml2json');
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 var port = process.env.PORT || 8080;
 
