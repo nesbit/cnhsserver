ed53809e198011740dfbf8d21b0d497ac24d2d89
nesbit <nesbit@users.noreply.github.com>
2016-09-21 21:24:43 -0700

    Update webface.js

--- webface.js
+++ webface.js
@@ -75,17 +75,17 @@ GET IPHONE ADDRESS
 ===================================================*/
 app.get('/ios/:query', function(req, res) {
   console.log(req.params.query);
- var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'armadillodude',
-  password : '',
-  database : 'deviceid'
+var connection = mysql.createConnection({
+  host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'oi7hss5h1e0qnfb7',
+  password : 'hpsj2fsktv68mi1d',
+  database : 'j2rymydc2j3ul9jp'
 });
 var connection2 = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'armadillodude',
-  password : '',
-  database : 'deviceid'
+  host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'oi7hss5h1e0qnfb7',
+  password : 'hpsj2fsktv68mi1d',
+  database : 'j2rymydc2j3ul9jp'
 });
   
 connection.query("SELECT id from `registrationIDIOS` WHERE id = '" + req.params.query + "'", function(err, rows, fields) {
